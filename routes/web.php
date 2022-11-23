250b45cccd8cdfe8ff140573716846c74f9148dd
Mathison-Carla <carla@mathisonprojects.com>
2022-11-23 17:49:50 -0300

    deleted require dir in web.php
    line 23

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
 
 Route::get ('any', function() {
     return view ('app');
