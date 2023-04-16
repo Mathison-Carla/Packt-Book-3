8c32e04a2ef459f4ec68f2a0ee501eae78685b20
Rodrigo Arrua <rahacker2006@gmail.com>
2023-04-16 09:22:31 -0400

    Mejoras en el frontend

--- resources/js/components/flights/FlightIndex.vue
+++ resources/js/components/flights/FlightIndex.vue
@@ -46,12 +46,15 @@
                                     <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
                                         {{ flight.airline }}
                                     </td>
-                                    <router-link :to="{ name: 'flight.edit', params: { id: flight.id } }"
-                                                 class="mr-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                                        Edit</router-link>
+
                                     <td class="px-6 py-4 text-sm text-center leading-5 text-gray-900 whitespace-no-wrap">
+
+                                            <router-link :to="{ name: 'flight.edit', params: { id: flight.id } }"
+                                                         class="mr-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                                Edit</router-link>
+
                                         <button @click="deleteFligth(flight.id)"
-                                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                                             Delete
                                         </button>
                                     </td>
